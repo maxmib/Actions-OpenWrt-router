fc7e9f775f859c7dcc882318027e35c7f08376fc
maxmib <30944505+maxmib@users.noreply.github.com>
2020-04-23 23:36:25 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,3 +7,7 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/\"#src-git\"/\"src-git\"/g' feeds.conf.default;
+./scripts/feeds clean;
+./scripts/feeds update -a;
+./scripts/feeds install -a;
